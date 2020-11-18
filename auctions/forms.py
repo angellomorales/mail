c70f6bcef23deb933227c0a351990dc9aba186ff
angellomorales <ing.angellomorales@gmail.com>
2020-11-18 00:01:54 -0500

    change the css to enhance the view

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import CategoryChoices
 
-
+#Multiple ways to add class to forms
 class NewListingForm(forms.Form):
     title = forms.CharField(required=True, label="Title")
     price = forms.DecimalField(decimal_places=2, required=True, label="Price")
@@ -11,17 +11,37 @@ class NewListingForm(forms.Form):
     description = forms.CharField(
         widget=forms.Textarea, required=False, label="Description")
 
+    def __init__(self, *args, **kwargs):
+        super(NewListingForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['price'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['imageURL'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['category'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2'})
+        self.fields['description'].widget.attrs.update(
+            {'class': 'form-control mx-sm-3 mb-2 txtfield'})
+
 
 class NewBidForm(forms.Form):
     bid = forms.DecimalField(
         decimal_places=2, required=True, min_value=1, label="Bid")
+    bid.widget.attrs.update({'class': 'form-control mx-sm-2'})
 
 
 class NewCommentForm(forms.Form):
     comments = forms.CharField(
-        widget=forms.Textarea, required=True, label="Your comment")
+        widget=forms.Textarea(attrs={'class' : 'form-control sm-3 mb-2 txtfield'}), required=True, label="Your comment")
+    
 
 
 class NewCategoryForm(forms.Form):
     category = forms.ChoiceField(
         choices=CategoryChoices.choices, required=True, label="Category")
+
+    def __init__(self, *args, **kwargs):
+        super(NewCategoryForm, self).__init__(*args, **kwargs)
+        self.fields['category'].widget.attrs.update(
+            {'class': 'form-control mx-sm-2'})
