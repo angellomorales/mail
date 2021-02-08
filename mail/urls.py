a702d040421050f5e576e697476f7f7fe89d268f
angellomorales <ing.angellomorales@gmail.com>
2021-02-08 17:40:09 -0500

    first commit

--- mail/urls.py
+++ mail/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("new", views.new, name="new"),
-    path("listing/<int:item_id>", views.listing, name="listing"),
-    path("watchlist", views.watchlist, name="watchlist"),
-    path("categories", views.categories, name="categories"),
 
+    # API Routes
+    path("emails", views.compose, name="compose"),
+    path("emails/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
 ]
