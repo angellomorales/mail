6d24c472aa06c294176ecad0251611a5fe0759d1
angellomorales <ing.angellomorales@gmail.com>
2021-02-11 11:03:28 -0500

    enhance admin site for email

--- mail/admin.py
+++ mail/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Email,User 
 
 # Register your models here.
+class EmailAdmin(admin.ModelAdmin):
+    list_display=("id","sender","get_recipients","subject","timestamp","read","archived")
 
 admin.site.register(User)
-admin.site.register(Email)
+admin.site.register(Email,EmailAdmin)
