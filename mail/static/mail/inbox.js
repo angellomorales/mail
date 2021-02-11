28d4e35c4621d2a6e7524e75dbd774433e163eaf
angellomorales <ing.angellomorales@gmail.com>
2021-02-11 14:57:49 -0500

    mailbox feature with gray background color in read mails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,4 @@
+let mailboxValue;
 document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
@@ -64,21 +65,43 @@ function manage_form_fields(formelement) {
 }
 
 function load_mailbox(mailbox) {
-
+  mailboxValue = mailbox;
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  
-  //request to their respective mailbox
-  // fetch('/emails/inbox')
-  //   .then(response => response.json())
-  //   .then(emails => {
-  //     // Print emails
-  //     console.log(emails);
 
-  //     // ... do something else with emails ...
-  //   });
+  //request to their respective mailbox
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      // Print emails
+      console.log(`${mailbox}:`);
+      console.log(emails);
+      emails.forEach(add_mail);
+
+      // ... do something else with emails ...
+    });
 }
+
+function add_mail(contents) {
+
+  let title = contents.sender;
+  if (mailboxValue == 'sent') {
+    title = contents.recipients;
+  }
+
+  // create new mail container 
+  const mailContainer = document.createElement('div');
+  mailContainer.className = 'mailContainer';
+  mailContainer.innerHTML = `<div><small>${contents.timestamp}</small></div><h5>${title}</h5><p>${contents.subject}</p>`;
+  if (contents.read) {
+    mailContainer.style.backgroundColor='gray';
+    mailContainer.style.color='white';
+  }
+
+  // Add mail to div emails-view 
+  document.querySelector('#emails-view').append(mailContainer);
+};
